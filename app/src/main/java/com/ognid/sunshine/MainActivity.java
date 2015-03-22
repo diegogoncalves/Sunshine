8eba0933cabc2bf23034ee5f110611391e3be18f
diego_goncalves <ddiegofg@gmail.com>
2015-03-22 18:15:39 -0300

    retrieve real weather forecast

--- app/src/main/java/com/ognid/sunshine/MainActivity.java
+++ app/src/main/java/com/ognid/sunshine/MainActivity.java
@@ -17,10 +17,18 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -65,53 +73,62 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
-        Activity activity;
-
-        public PlaceholderFragment(Activity ac) {
-            activity=ac;
-        }
-
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            List<String> weekForecast=getForecast();
-
-            ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,weekForecast);
-
-            ListView listView=(ListView) rootView.findViewById(R.id.listView_forecast);
-            listView.setAdapter(adapter);
+            getForecast();
 
             return rootView;
         }
 
-        List<String> getForecast(){
+        void getForecast(){
             ConnectivityManager connMgr = (ConnectivityManager)
-                    activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
-                DownloadForecast download=new DownloadForecast();
-
+                new DownloadForecast(getActivity()).execute();
             } else {
-                // display error
+                // display error test b
+                Log.d("problem","Houston we have a problem");
             }
         }
 
 
     }
 
-    private class DownloadForecast extends AsyncTask<String,Void,String>{
+    public static class DownloadForecast extends AsyncTask<Void,Void,List<String>>{
 
-        @Override
-        protected String doInBackground(String... city) {
-            return download(city);
+        Activity activity2;
+
+        DownloadForecast(Activity ac){
+            activity=ac;
         }
 
-        private String download(String city) throws IOException {
+        @Override
+        protected List<String> doInBackground(Void ... f) {
+            try{
+                return download();
+            }
+            catch (IOException e){
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<String> weekForecast) {
+            super.onPostExecute(weekForecast);
+
+            ArrayAdapter<String> adapter=new ArrayAdapter<String>(activity,R.layout.list_item_forecast,R.id.list_item_forecast_textView,weekForecast);
+            ListView listView=(ListView) activity.findViewById(R.id.listView_forecast);
+            listView.setAdapter(adapter);
+        }
+
+        private List<String> download() throws IOException {
             InputStream is=null;
             try{
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?id=3451190&cnt=10&mode=json&units=metric&APPID=43d651f8e1f3bb5f9b38ba0a16da7087");
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=Rio de Janeiro, BR&cnt=7&mode=json&units=metric&APPID=43d651f8e1f3bb5f9b38ba0a16da7087");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setReadTimeout(10000 /* milliseconds */);
                 conn.setConnectTimeout(15000 /* milliseconds */);
@@ -120,17 +137,55 @@ public class MainActivity extends ActionBarActivity {
                 // Starts the query
                 conn.connect();
                 int response = conn.getResponseCode();
-                Log.d(DEBUG_TAG, "The response is: " + response);
+                Log.d("response",Integer.toString(response));
+
                 is = conn.getInputStream();
 
                 // Convert the InputStream into a string
-                String contentAsString = readIt(is, len);
-                return contentAsString;
+                String contentAsString = readIt(is);
+                Log.d("string",contentAsString);
+                return parse(contentAsString);
 
             }
             finally {
                 if(is!=null)is.close();
             }
         }
+
+        public String readIt(InputStream is) throws IOException, UnsupportedEncodingException {
+            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+            return s.hasNext() ? s.next() : "";
+        }
+
+        List<String> parse(String s){
+            try{
+                Log.d("string2",s);
+                JSONObject json =new JSONObject(s);
+                JSONArray array=json.getJSONArray("list");
+                List<String>list=new LinkedList<>();
+
+                Log.d("lengthArray",Integer.toString(array.length()));
+                for(int i=0;i<array.length();i++){
+
+
+                    String weatherCondition=array.getJSONObject(i).getJSONArray("weather").getJSONObject(0).getString("main");
+                    if(i==0)  list.add("Today- "+weatherCondition);
+                    else {
+                        Calendar c = Calendar.getInstance();
+                        c.add(Calendar.DATE, i);
+                        String weekDay=new SimpleDateFormat("EE").format(c.getTime());
+                        list.add(weekDay+"- "+weatherCondition);
+                    }
+                }
+                Log.d("lengthList",Integer.toString(list.size()));
+                return list;
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
     }
 }
