ab1914de678ea5859072c2c08df87d2447092b7d
diego_goncalves <ddiegofg@gmail.com>
2015-03-27 00:39:22 -0300

    lesson 2 completed

--- app/src/main/java/com/ognid/sunshine/MainActivity.java
+++ app/src/main/java/com/ognid/sunshine/MainActivity.java
@@ -1,19 +1,9 @@
 package com.ognid.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,34 +42,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            //TO-DO
-            List<String> weekForecast=new ArrayList<>(Arrays.asList(new String[]{"Rain","Sun","Cloud"}));
-
-            ArrayAdapter<String> adapter=new ArrayAdapter<>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,weekForecast);
-
-            ListView listView=(ListView) rootView.findViewById(R.id.listView_forecast);
-            listView.setAdapter(adapter);
-
-            return rootView;
-        }
-
-
-
-
-    }
 
 }
